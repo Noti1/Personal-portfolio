c19eb75538db8ebc2e41a9fc65fd4cfee9f74de6
ramzi <ramzyally23@gmail.com>
2024-02-03 09:40:57 -0800

    just finished creating my personal portfolio

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+      <p className='copyright'>©2024</p> <p  className='motto'> always in solution mode </p>
+       </footer> 
+    </div>
+  )
+}
+
+export default Footer
